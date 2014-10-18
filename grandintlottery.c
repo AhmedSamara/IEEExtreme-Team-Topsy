c8c9ef8a5cf05c7d5027a5dae872b3434880f228
Ahmed Samara <asamara@ncsu.edu>
2014-10-18 16:52:24 -0400

    fixed everything

--- grandintlottery.c
+++ grandintlottery.c
@@ -18,36 +18,32 @@ int main() {
 
     int picks[N];
     int a;
+    //scan in picks.
     for(a=0; a < N; a++){
-        scanf("%d\n", &picks[a]);
+        scanf("%d", &picks[a]);
     }
 
-    int lotto_sequence[9999];
-    //numbers can't be 0, so will use this to mark end.
+
+    int contain_count = 0;
+    int seq_length = max_val - min_val;
+    //default to max possible length.
+    int lotto_sequence[seq_length];
     int i;
-    for(i=0; i<9999; i++){
-        lotto_sequence[i]=0;
-    }
-    int lotto_index=0;
-    //generate lotto sequence.
-    int candidate_number;
-    int j=0;
-    for(candidate_number = min_val;
-        candidate_number<= max_val;
-         candidate_number++){
-        //check if any of picks are contains.
-        for(j=0;j<N;j++){
-            if (
-                contains_number(candidate_number,picks[j])
-                ){
-                    lotto_sequence[lotto_index++] = candidate_number;
-                }
+
+    for(a= min_val; a<=max_val; a++){
+        //iterates through all numbers that may be added to lotto_sequence.
+        for(i=0;i<N;i++){
+         //   if(a < picks[i]) continue;
+            if (contains_number(a,picks[i])){
+                //store val, keep track of size.
+                lotto_sequence[contain_count++] = a;
+                //printf("%d\n",a);
+            }
         }
     }
 
 
-
-    printf("%d",lotto_sequence[winner_i]);
+    printf("%d",lotto_sequence[winner_i-1]);
 
     return 0;
 }
@@ -59,6 +55,7 @@ int contains_number(int container, int b){
    //First, figure out the power of b to figure out how to do it.
     int x;
     //finds number of digits to compare.
+
     x = pow(10,find_power(b));
     while(container > 0){
         //if b is contained.
@@ -71,13 +68,26 @@ int contains_number(int container, int b){
 }
 
 int find_power(int number){
-    int n = 0;
+/*
+    if(number < 10)
+        return 10;
+    else if (number < 100){
+        return 100;
+    } else if (number < 1000)
+        return 1000;
+    else if (number < 10000)
+        return 10000;
+    else if(number < 100000)
+        return 100000;
+    else if (number < 1000000)
+        return 1000000;
+    else return 10000000; */
+        int n=0;
     while(1){
         if(number < pow(10,n)){
             break;
         }
         n++;
     }
-
     return n;
 }
