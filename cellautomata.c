627b48487c206a126666175481510c912b91256d
Ahmed Samara <asamara@ncsu.edu>
2014-10-18 10:34:48 -0400

    now deals with stable state cases

--- cellautomata.c
+++ cellautomata.c
@@ -2,24 +2,25 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
-int N = 3;
-void print_as_asterix(int* , int );
-
+int N;
+void print_as_asterix(int[] , int );
+int new_value(int[], int );
+int row =1;
 int main() {
     int rule;
     int max_iter;
     int initial;
+
     scanf("%d %d %d %d", &rule, &max_iter, &N, &initial);
 
     int binary_rep[N];
     //initialize binary rep to zero.
     //appropriate bits will be set to 1.
-    printf("enter an initial value\n");
    // scanf("%d",&initial);
     int a = 1;
     int i;
     //convert value to binary array representation.
-    for(i=0; i < N; i++){
+    for(i=N-1; i >=0; i--){
         binary_rep[i] = (initial & a) && 1;
 
         a = a << 1;
@@ -29,14 +30,21 @@ int main() {
     //
     int new_binary[N];
 
-
-    for (max_iter; max_iter >0; max_iter--){
+    int j=0;
+    for (j=0; j < max_iter-1; j++){
+        int stable = 1;
         //create the next line.
         for(i=0; i < N; i++){
             new_binary[i] = new_value(binary_rep,i);
             //printf("%d",new_binary[i]);
+            //assume that it's stable until proven otherwise.
+            if(new_binary[i] != binary_rep[i])
+                stable = 0;
+
         }
+        if(stable) break;
         print_as_asterix(new_binary,N);
+        //once it's stable, done.
 
         //Make the "new" line the previousline.
         for(i=0; i < N; i++){
@@ -49,8 +57,16 @@ int main() {
 
     return 0;
 }
-void print_as_asterix(int* arr, int length){
+void print_as_asterix(int arr[], int length){
     int x;
+
+    if(row < 10){
+        printf("%d   -",row++);
+    } else if(row <100){
+        printf("%d  -",row++);
+    } else if(row , 1000){
+        printf("%d -",row++);
+    }
     for(x=0; x<length;x++){
         if(arr[x]){
             printf("*");
@@ -58,12 +74,13 @@ void print_as_asterix(int* arr, int length){
             printf(" ");
         }
     }
-    printf("\n");
+    printf("-\n");
 }
 
-int new_value(int*rep, int index){
+int new_value(int rep[], int index){
     int nm1, np1, n;
     //corner cases.
+    n = rep[index];
     if(index == 0){
         nm1 = 0;
     }else{
@@ -74,23 +91,12 @@ int new_value(int*rep, int index){
     } else{
         np1 = rep[index+1];
     }
-    n = rep[index];
+
 
     int a,b,c;
     a= nm1;
     b= rep[index];
     c = np1;
     return (!a&&c)||(!a&&b)||(a&&!b&&!c);
-
-    /*
-    if(nm1 && n && np1){
-        return 0;
-    }
-     else if (nm1 && n && !np1){
-        return 0;
-     }
-     else if(nm1 && !n && )
-
-    return */
 
 }
