a2ebe7042e13e2a4354614e02df7381e7694977b
Ahmed Samara <asamara@ncsu.edu>
2014-10-18 06:51:22 -0400

    Working new_version function

---
+++ cellautomata.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int N = 3;
+int main() {
+    int rule;
+    int max_iter;
+    int initial;
+  //  scanf("%d %d %d %d", &rule, &max_iter, &N, &initial);
+
+    int binary_rep[N];
+    //initialize binary rep to zero.
+    //appropriate bits will be set to 1.
+    printf("enter an initial value\n");
+   // scanf("%d",&initial);
+    int a = 1;
+    int i;
+    //convert value to binary array representation.
+    for(i=0; i < N; i++){
+        binary_rep[i] = (initial & a) && 1;
+        printf("%d", binary_rep[i]);
+        a = a << 1;
+    }
+    printf("\n");
+
+    //
+    int new_binary[N];
+    for(i=0; i < N; i++){
+        new_binary[i] = new_value(binary_rep,i);
+        printf("%d",new_binary[i]);
+    }
+    printf("\n");
+
+
+
+    return 0;
+}
+
+int new_value(int*rep, int index){
+    int nm1, np1, n;
+    //corner cases.
+    if(index == 0){
+        nm1 = 0;
+    }else{
+        nm1 = rep[index-1];
+    }
+    if(index == N-1){
+        np1 = 0;
+    } else{
+        np1 = rep[index+1];
+    }
+    n = rep[index];
+
+    int a,b,c;
+    a= nm1;
+    b= rep[index];
+    c = np1;
+    return (!a&&c)||(!a&&b)||(a&&!b&&!c);
+
+    /*
+    if(nm1 && n && np1){
+        return 0;
+    }
+     else if (nm1 && n && !np1){
+        return 0;
+     }
+     else if(nm1 && !n && )
+
+    return */
+
+}
